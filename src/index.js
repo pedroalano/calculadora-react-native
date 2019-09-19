2c18b00dc44cb928ead2c88cc110c606ccb7ffc5
Pedro Alano <pedroalano@gmail.com>
2019-09-19 15:20:21 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { View , Text} from 'react-native';
+
+
+export default App = () => {
+    
+        return (
+            <View>
+                <Text>Calculator</Text>
+            </View>
+        );
+    
+}
